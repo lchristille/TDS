0f779a23ef5df3d88a1a0b39125af58f5c356b62
felim.black@gmail.com <ac234cnE>
2015-12-04 13:33:30 +0100

    Enabling "Tutti" radiobutton

--- app/src/main/java/it/oltrenuovefrontiere/tds/view/SheetTableView.java
+++ app/src/main/java/it/oltrenuovefrontiere/tds/view/SheetTableView.java
@@ -30,7 +30,7 @@ public class SheetTableView {
     TableLayout technicalList;
 
     public SheetTableView(MainActivity mainActivity, Cursor cursor) {
-        Context newct = (Context) mainActivity;
+        Context newct = mainActivity;
         SheetTableView(newct, cursor);
     }
 
@@ -42,70 +42,69 @@ public class SheetTableView {
 
 
     public TableLayout makeTable() {
-         technicalList = new TableLayout(ct);
-         adapter.open();
+        technicalList = new TableLayout(ct);
+        adapter.open();
 
-                if (cursor.getCount() > 1) {
-                    cursor.moveToFirst();
-                    do {
-                        TableRow row = new TableRow(ct);
-                        final Button t = new Button(ct);
-                        t.setOnClickListener(new View.OnClickListener() {
-                            String _path;
-                            Context _ct;
+        if (cursor.getCount() > 1) {
+            cursor.moveToFirst();
+            do {
+                TableRow row = new TableRow(ct);
+                final Button t = new Button(ct);
+                t.setOnClickListener(new View.OnClickListener() {
+                    String _path;
+                    Context _ct;
 
-                            @Override
-                            public void onClick(View v) {
-                                File pdfToOpen = new File(_path);
-                                Intent i = new Intent();
-                                i.setAction(android.content.Intent.ACTION_VIEW);
-                                i.setDataAndType(Uri.fromFile(pdfToOpen), "application/pdf");
-                                ct.startActivity(i);
-                            }
+                    @Override
+                    public void onClick(View v) {
+                        File pdfToOpen = new File(_path);
+                        Intent i = new Intent();
+                        i.setAction(android.content.Intent.ACTION_VIEW);
+                        i.setDataAndType(Uri.fromFile(pdfToOpen), "application/pdf");
+                        ct.startActivity(i);
+                    }
 
-                            private View.OnClickListener init(String _path, Context _ct) {
-                                this._path = _path;
-                                this._ct = _ct;
-                                return this;
-                            }
+                    private View.OnClickListener init(String _path, Context _ct) {
+                        this._path = _path;
+                        this._ct = _ct;
+                        return this;
+                    }
 
-                        }.init(cursor.getString(2), ct));
-                        String backgroundPath = cursor.getString(2).replace(".pdf", ".jpg");
-                        if (new File(backgroundPath).isFile()) {
-                            Bitmap image = new BitmapFactory().decodeFile(backgroundPath);
-                            int originalWidth = image.getWidth();
-                            int originalHeight = image.getHeight();
-                            int newWidth = -1;
-                            int newHeight = -1;
-                            float multFactor = -1.0F;
-                            if (originalHeight > originalWidth) {
-                                newHeight = 150;
-                                multFactor = (float) originalWidth / (float) originalHeight;
-                                newWidth = (int) (newHeight * multFactor);
-                            } else if (originalWidth > originalHeight) {
-                                newWidth = 150;
-                                multFactor = (float) originalHeight / (float) originalWidth;
-                                newHeight = (int) (newWidth * multFactor);
-                            } else if (originalHeight == originalWidth) {
-                                newHeight = 150;
-                                newWidth = 150;
-                            }
-                            image = Bitmap.createScaledBitmap(image, newWidth, newHeight, true);
-                            t.setBackground(new BitmapDrawable(image));
-                        }
-                        TextView txt = new TextView(ct);
-                        txt.setText(cursor.getString(1).replace(".pdf", ""));
-                        row.addView(t);
-                        row.addView(txt);
-                        technicalList.addView(row, new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT));
-                        cursor.moveToNext();
-                    } while (!cursor.isLast());
+                }.init(cursor.getString(2), ct));
+                String backgroundPath = cursor.getString(2).replace(".pdf", ".jpg");
+                if (new File(backgroundPath).isFile()) {
+                    Bitmap image = new BitmapFactory().decodeFile(backgroundPath);
+                    int originalWidth = image.getWidth();
+                    int originalHeight = image.getHeight();
+                    int newWidth = -1;
+                    int newHeight = -1;
+                    float multFactor = -1.0F;
+                    if (originalHeight > originalWidth) {
+                        newHeight = 150;
+                        multFactor = (float) originalWidth / (float) originalHeight;
+                        newWidth = (int) (newHeight * multFactor);
+                    } else if (originalWidth > originalHeight) {
+                        newWidth = 150;
+                        multFactor = (float) originalHeight / (float) originalWidth;
+                        newHeight = (int) (newWidth * multFactor);
+                    } else if (originalHeight == originalWidth) {
+                        newHeight = 150;
+                        newWidth = 150;
+                    }
+                    image = Bitmap.createScaledBitmap(image, newWidth, newHeight, true);
+                    t.setBackground(new BitmapDrawable(image));
                 }
-                cursor.close();
+                TextView txt = new TextView(ct);
+                txt.setText(cursor.getString(1).replace(".pdf", ""));
+                row.addView(t);
+                row.addView(txt);
+                technicalList.addView(row, new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT));
+                cursor.moveToNext();
+            } while (!cursor.isLast());
+        }
+        cursor.close();
 
-                adapter.close();
-                //progress.dismiss();
-        });
+        adapter.close();
+        //progress.dismiss();
         return technicalList;
     }
 }
